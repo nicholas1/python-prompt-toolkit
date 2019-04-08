c5b0e90f73138d7260def9e176de41aba17d8143
Jonathan Slenders <jonathan@slenders.be>
2019-04-08 20:29:32 +0200

    Sorting of imports (using `isort -m 3 -tc`).

--- prompt_toolkit/filters/cli.py
+++ prompt_toolkit/filters/cli.py
@@ -1,7 +1,5 @@
 from __future__ import unicode_literals
 
-# For backwards-compatibility. keep this file.
-# (Many people are going to have key bindings that rely on this file.)
 from .app import *
 
 __all__ = [
